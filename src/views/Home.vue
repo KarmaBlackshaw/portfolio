4aa47569c8463a0000d0d4f0e0325744a0997ddd
Karma Blackshaw <irenesejah29@gmail.com>
2021-08-20 13:37:50 +0800

    add floating btn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,9 +20,21 @@
         <a
           class="subtitle--vue"
           href="https://vuejs.org/"
-        >Vue</a>.
+        >Vue</a> and
+
+        <a
+          class="subtitle--node"
+          href="https://nodejs.org/en/about/"
+        >Node</a>.
       </p>
     </div>
+
+    <button class="btn-down">
+      <img
+        src="@/assets/svg/icons/chevron-down-solid.svg"
+        alt=""
+      >
+    </button>
   </section>
 </template>
 
@@ -43,44 +55,85 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 
-  .home-hero--banner {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 10px;
+  .home--container {
+    .home-hero--banner {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 10px;
 
-    .banner {
-      max-height: 200px;
-      width: auto;
+      .banner {
+        max-height: 200px;
+        width: auto;
+
+        @include xs {
+          max-height: 130px;
+        }
+      }
+    }
+
+    .home-hero--title {
+      text-align: center;
+      font-size: 4em;
+      font-weight: bold;
+      color: white;
 
       @include xs {
-      max-height: 130px;
+        font-size: 1.9em;
+      }
+
+      @include sm {
+        font-size: 3em;
+      }
+    }
+
+    .home-hero--subtitle {
+      text-align: center;
+      font-size: 1em;
+      font-weight: light;
+
+      .subtitle--vue {
+        color: #40b882;
+      }
+
+      .subtitle--node {
+        color: #76b55f;
       }
     }
   }
 
-  .home-hero--title {
-    text-align: center;
-    font-size: 4em;
-    font-weight: bold;
-    color: white;
-
-    @include xs {
-      font-size: 1.9em;
+  @keyframes float {
+    0% {
+      box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+      transform: translatey(0px);
     }
-
-    @include sm {
-      font-size: 3em;
+    50% {
+      box-shadow: 0 25px 15px 0px rgba(0, 0, 0, 0.2);
+      transform: translatey(-20px);
+    }
+    100% {
+      box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+      transform: translatey(0px);
     }
   }
 
-  .home-hero--subtitle {
-    text-align: center;
-    font-size: 1em;
-    font-weight: light;
+  .btn-down {
+    position: absolute;
+    height: 70px;
+    width: 70px;
+    border: 1px solid white;
+    bottom: 50px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    animation: float 6s ease-in-out infinite;
 
-    .subtitle--vue {
-      color: #40B882;
+    img {
+      height: 30px;
+      width: 30px;
     }
   }
 }
