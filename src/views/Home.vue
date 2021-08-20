6209eaac275ba3db3bbb9d7baa1b76080642b4ac
Karma Blackshaw <irenesejah29@gmail.com>
2021-08-20 14:47:33 +0800

    add text stretch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,52 @@
 <template>
-  <section class="home">
-    <div class="home--container">
-      <div class="home-hero--banner">
-        <code-think class="banner" />
-        <!-- <img
+  <article>
+    <section class="hero">
+      <div class="hero--container">
+        <div class="hero--banner">
+          <code-think class="banner" />
+          <!-- <img
           src="@/assets/svg/code-think-css.svg"
           alt=""
         > -->
+        </div>
+
+        <div class="hero--banner-title">
+          <template v-for="(currLetter, letterKey) in hero.title">
+            <span
+              v-if="currLetter !== ' '"
+              :key="letterKey"
+            >{{ currLetter }}</span>
+            <span
+              v-else
+              :key="letterKey"
+            >&nbsp;</span>
+          </template>
+        </div>
+
+        <p class="hero--banner-subtitle">
+          I am a web developer from Philippines and I enjoy building web applications,
+          usually with
+
+          <a
+            class="subtitle--vue"
+            href="https://vuejs.org/"
+          >Vue</a> and
+
+          <a
+            class="subtitle--node"
+            href="https://nodejs.org/en/about/"
+          >Node</a>.
+        </p>
       </div>
 
-      <p class="home-hero--title">
-        Hi, I'm Ernie Jeash!
-      </p>
-
-      <p class="home-hero--subtitle">
-        I am a web developer from Philippines and I enjoy building web applications,
-        usually with
-
-        <a
-          class="subtitle--vue"
-          href="https://vuejs.org/"
-        >Vue</a> and
-
-        <a
-          class="subtitle--node"
-          href="https://nodejs.org/en/about/"
-        >Node</a>.
-      </p>
-    </div>
-
-    <button class="btn-down">
-      <img
-        src="@/assets/svg/icons/chevron-down-solid.svg"
-        alt=""
-      >
-    </button>
-  </section>
+      <button class="btn-down">
+        <img
+          src="@/assets/svg/icons/chevron-down-solid.svg"
+          alt=""
+        >
+      </button>
+    </section>
+  </article>
 </template>
 
 <script>
@@ -45,20 +56,85 @@ export default {
 
   components: {
     CodeThink
+  },
+
+  data () {
+    return {
+      hero: {
+        title: 'Hi, I\'m Ernie Jeash!'
+      },
+
+      contacts: [
+        {
+          text: 'Github',
+          value: 'https://github.com/KarmaBlackshaw'
+        },
+        {
+          text: 'LinkedIn',
+          value: 'https://www.linkedin.com/in/ernie-jeash-villahermosa-576187199/'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.home {
+@keyframes float {
+  0% {
+    box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+    transform: translatey(0px);
+  }
+  50% {
+    box-shadow: 0 25px 15px 0px rgba(0, 0, 0, 0.2);
+    transform: translatey(-10px);
+  }
+  100% {
+    box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+    transform: translatey(0px);
+  }
+}
+
+@keyframes stretch {
+  0% {
+    transform: scale(1, 1);
+  }
+
+  40% {
+    transform: scale(1.2, 0.5);
+  }
+
+  60% {
+    transform: scale(0.8, 1.5);
+  }
+
+  70% {
+    transform: scale(1.1, 0.9);
+  }
+
+  80% {
+    transform: scale(0.9, 1.3);
+  }
+
+  90% {
+    transform: scale(1, 0.9);
+  }
+
+  90% {
+    transform: scale(1, 1);
+  }
+}
+
+.hero {
   height: calc(100vh - 64px);
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
+  user-select: none;
 
-  .home--container {
-    .home-hero--banner {
+  .hero--container {
+    .hero--banner {
       display: flex;
       justify-content: center;
       margin-bottom: 10px;
@@ -73,11 +149,20 @@ export default {
       }
     }
 
-    .home-hero--title {
+    .hero--banner-title {
       text-align: center;
-      font-size: 4em;
       font-weight: bold;
-      color: white;
+      color: rgb(224, 224, 224);
+      font-size: 4em;
+
+      span {
+        display: inline-block;
+
+        &:hover {
+          animation: stretch 0.8s ease-in-out;
+          color: #fff;
+        }
+      }
 
       @include xs {
         font-size: 1.9em;
@@ -88,7 +173,7 @@ export default {
       }
     }
 
-    .home-hero--subtitle {
+    .hero--banner-subtitle {
       text-align: center;
       font-size: 1em;
       font-weight: light;
@@ -100,21 +185,6 @@ export default {
       .subtitle--node {
         color: #76b55f;
       }
-    }
-  }
-
-  @keyframes float {
-    0% {
-      box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
-      transform: translatey(0px);
-    }
-    50% {
-      box-shadow: 0 25px 15px 0px rgba(0, 0, 0, 0.2);
-      transform: translatey(-20px);
-    }
-    100% {
-      box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
-      transform: translatey(0px);
     }
   }
 
@@ -134,6 +204,17 @@ export default {
     img {
       height: 30px;
       width: 30px;
+    }
+
+    @include xs {
+      height: 50px;
+      width: 50px;
+      bottom: 30px;
+
+      img {
+        height: 20px;
+        width: 20px;
+      }
     }
   }
 }
