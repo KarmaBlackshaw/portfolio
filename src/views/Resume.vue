dba1e4596e5567ac4446f6ecf3bfae610f504dec
Karma Blackshaw <irenesejah29@gmail.com>
2021-10-09 18:43:27 +0800

    separete component scss

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,10 +3,7 @@
     id="resume"
     class="resume"
   >
-    <div
-      v-if="$env.NODE_ENV === 'development'"
-      class="resume-container page-container"
-    >
+    <div class="resume-container page-container">
       <h1 class="page-title">
         CHECK MY RESUME
       </h1>
@@ -15,27 +12,76 @@
         Resume
       </base-section-title>
 
-      <div class="resume-container">
-        <div class="resume-container--title">
-          Professional Experience
+      <div class="resume-content">
+        <div class="resume-content-item">
+          <div class="content--title">
+            Professional Experience
+          </div>
+
+          <div class="content--container">
+            <div
+              v-for="(currResume, resumeKey) in resumeList"
+              :key="resumeKey"
+              class="resume-item"
+            >
+              <div class="resume-item--title">
+                {{ currResume.title }}
+              </div>
+
+              <div class="resume-item--company">
+                {{ currResume.company }}
+              </div>
+
+              <div class="resume-item--duration">
+                {{ currResume.duration }}
+              </div>
+
+              <div class="resume-item--roles">
+                <ul>
+                  <li
+                    v-for="(currTask, taskKey) in currResume.tasks"
+                    :key="taskKey"
+                  >
+                    {{ currTask }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div class="resume-container--items">
-          <div class="resume-item">
-            <div class="resume-item--title">
-              Fullstack Web Developer
-            </div>
-            <div class="resume-item--date">
-              July 2019 - Present
-            </div>
-            <div class="resume-item--company">
-              Cubic Solutions
-            </div>
-            <div class="resume-item--roles">
-              <ul>
-                <li>Research, design, develop, document software for clients</li>
-                <li>Mentor interns</li>
-              </ul>
+        <div class="resume-content-item">
+          <div class="content--title">
+            Educational Details
+          </div>
+
+          <div class="content--container">
+            <div class="resume-item">
+              <div class="resume-item--title">
+                Bachelor of Science in Information Technology
+              </div>
+
+              <div class="resume-item--company">
+                Leyte Normal University
+              </div>
+
+              <div class="resume-item--duration">
+                June 2015 - September 2019
+              </div>
+
+              <div
+                v-if="0"
+                class="resume-item--roles"
+              >
+                <ul>
+                  <li
+                    v-for="(currTask, taskKey) in currResume.tasks"
+                    :key="taskKey"
+                  >
+                    {{ currTask }}
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -50,18 +96,40 @@ export default {
 
   data () {
     return {
-
+      resumeList: [
+        {
+          title: 'Fullstack Web Developer',
+          company: 'Cubic Solutions',
+          duration: 'July 2019 - Present',
+          tasks: [
+            'Research, design, develop, and document software for clients',
+            'Improve existing codebase',
+            'Develop REST APIs',
+            'Mentor new employees'
+          ]
+        },
+        {
+          title: 'Freelance',
+          duration: 'August 2018 - Present',
+          tasks: [
+            'Research, design, develop, and document software for clients'
+          ]
+        },
+        {
+          title: 'Intern Web Developer',
+          company: 'Philhealth',
+          duration: 'May 2018 - August 2018',
+          tasks: [
+            'Research, design, and develop web applications',
+            'Automate process'
+          ]
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.resume {
-  padding-top: 100px;
-  background: white;
-  min-height: 100vh;
-  position: relative;
-  border-bottom: 1px solid #e1e2e4;
-}
+@import '@/assets/scss/views/resume';
 </style>
