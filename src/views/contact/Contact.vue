637fe23fa8b6bf02d29693790829a43b1dc28c3a
Karma Blackshaw <irenesejah29@gmail.com>
2021-12-16 16:47:07 +0800

    add send message in contacts

--- src/views/contact/Contact.vue
+++ src/views/contact/Contact.vue
@@ -22,7 +22,10 @@
           </div>
 
           <div class="card__body">
-            <div class="card__email">
+            <form
+              class="card__email"
+              @submit.prevent="handleFormSubmit"
+            >
               <div
                 class="email__item"
                 data-aos="fade-right"
@@ -31,10 +34,13 @@
                 <div class="email__label">
                   Name <span class="label__muted">(optional)</span>
                 </div>
+
                 <div class="email__input">
                   <input
+                    v-model="forms.email.data.from_name"
                     type="text"
                     placeholder="Karma Blackshaw"
+                    @input="handleOnInput"
                   >
                 </div>
               </div>
@@ -50,8 +56,10 @@
 
                 <div class="email__input">
                   <input
+                    v-model="forms.email.data.from_email"
                     type="text"
                     placeholder="johndoe@gmail.com"
+                    @input="handleOnInput"
                   >
                 </div>
               </div>
@@ -67,22 +75,26 @@
 
                 <div class="email__input">
                   <textarea
+                    v-model="forms.email.data.message"
                     rows="8"
                     placeholder="Hi there!"
+                    @input="handleOnInput"
                   ></textarea>
                 </div>
               </div>
 
-              <div
-                class="email__item"
-                data-aos="fade-right"
-                data-aos-delay="300"
-              >
-                <button class="email__btn">
+              <div class="email__item">
+                <button
+                  data-aos="fade-right"
+                  data-aos-delay="300"
+                  class="email__btn"
+                  type="submit"
+                  :disabled="!forms.email.is_valid"
+                >
                   Send
                 </button>
               </div>
-            </div>
+            </form>
 
             <div class="card__divider"></div>
 
@@ -130,10 +142,28 @@
 </template>
 
 <script>
+import Joi from 'joi'
+
+import _debounce from 'lodash/debounce'
+import email from '@/config/email'
+
 export default {
   name: 'Contact',
 
   title: 'Contact',
+
+  data: () => ({
+    forms: {
+      email: {
+        is_valid: false,
+        data: {
+          from_name: undefined,
+          from_email: undefined,
+          message: undefined
+        }
+      }
+    }
+  }),
 
   computed: {
     contactDetails () {
@@ -154,6 +184,43 @@ export default {
           icon: 'phone-call.png'
         }
       ]
+    }
+  },
+
+  methods: {
+    handleOnInput: _debounce(function () {
+      const form = this.forms.email
+
+      const schema = Joi.object({
+        from_name: Joi.string()
+          .optional()
+          .label('Name'),
+        from_email: Joi.string()
+          .email({ tlds: { allow: false } })
+          .required()
+          .label('Email'),
+        message: Joi.string()
+          .required()
+          .label('Message')
+      })
+
+      schema.validateAsync(form.data)
+        .then(() => this.$set(this.forms.email, 'is_valid', true))
+        .catch(() => this.$set(this.forms.email, 'is_valid', false))
+    }, 300),
+
+    async handleFormSubmit () {
+      try {
+        const form = this.forms.email
+
+        await email.send({
+          from_name: form.data.from_name,
+          from_email: form.data.from_email,
+          message: form.data.message
+        })
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
