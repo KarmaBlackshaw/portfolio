7085c8cb0ce1bd675a6e0e3238e748d7ee33a3da
Karma Blackshaw <irenesejah29@gmail.com>
2021-12-30 17:43:11 +0800

    update vue config

--- src/assets/js/mixins/title.js
+++ src/assets/js/mixins/title.js
@@ -1,17 +1,19 @@
 function getTitle (vm) {
-  const { title } = vm.$options
+  const title = vm.$options.title
+
   if (title) {
     return typeof title === 'function'
       ? title.call(vm)
       : title
   }
 }
+
 export default {
   created () {
     const title = getTitle(this)
 
     if (title) {
-      document.title = `Ernie Jeash | ${title || 'Portfolio'}`
+      document.title = `${process.env.VUE_APP_TITLE} | ${title || 'Portfolio'}`
     }
   }
 }
